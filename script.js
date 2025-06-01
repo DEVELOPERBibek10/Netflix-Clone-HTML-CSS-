07b90f21a8f60e52b001db95a64158aa8b7f7362
GiruDev <girishpyakurel7@gmail.com>
2025-06-01 21:33:05 +0545

    update UI

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const scrollBox = document.querySelector(".movies");
 const leftScrollBtn = document.querySelector("#l-btn");
 const rightScrollBtn = document.querySelector("#r-btn");
 
+document.querySelectorAll(".movie").forEach(function (img) {
+  img.style.backgroundImage = `url(${img.dataset.id})`;
+});
+
 function scrollStart() {
   scrollBox.scrollLeft = 0;
   document.querySelector(".left").classList.add("clicked");
