917194dd7fac2e46b0a2f3b8119599074a0e9f46
GiruDev <girishpyakurel7@gmail.com>
2025-06-01 20:38:06 +0545

    update UI

---
+++ script.js
@@ -0,0 +1,15 @@
+const scrollBox = document.querySelector(".movies");
+const leftScrollBtn = document.querySelector("#l-btn");
+const rightScrollBtn = document.querySelector("#r-btn");
+
+function scrollStart() {
+  scrollBox.scrollLeft = 0;
+  document.querySelector(".left").classList.add("clicked");
+  document.querySelector(".right").classList.remove("clicked");
+}
+
+function scrollEnd() {
+  scrollBox.scrollLeft = scrollBox.scrollWidth;
+  document.querySelector(".right").classList.add("clicked");
+  document.querySelector(".left").classList.remove("clicked");
+}
